02f4edc812292de96be78bd576a59e1e6b492a29
logeshwaran9257@gmail.com <logeshwaran9257@gmail.com>
2025-08-15 03:46:02 +0530

    initial

--- src/main/java/com/jspiders/user_app/entity/User.java
+++ src/main/java/com/jspiders/user_app/entity/User.java
@@ -2,4 +2,9 @@ package com.jspiders.user_app.entity;
 
 public class User {
 
+	private int userId;
+	private String userName;
+	private String email;
+	private String password;
+	private String contact;
 }
